43a20acaa231a03d9984c3119a8e32150a35b091
LAPTOP-HFQ73LSO\HP <max99919991@gmail.com>
2018-01-02 17:18:06 +0200

    test

--- app/src/main/java/fatproject/activities/LoginActivity.java
+++ app/src/main/java/fatproject/activities/LoginActivity.java
@@ -1,11 +1,142 @@
 package fatproject.activities;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import fatproject.findatutor.R;
+import fatproject.validation.LoginValidator;
+import fatproject.validation.Validator;
 
 /**
  * Created by Victor on 31.12.2017.
  */
 
-public class LoginActivity  extends AppCompatActivity{
-    public static final String vitia = "dick";
+public class LoginActivity extends AppCompatActivity{
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @Bind(R.id.input_email) EditText _emailText;
+    @Bind(R.id.input_password) EditText _passwordText;
+    @Bind(R.id.btn_login) Button _loginButton;
+    @Bind(R.id.link_signup) TextView _signupLink;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        _signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), SingUpActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+    }
+
+    public boolean login() {
+        Log.d(TAG, "Login");
+
+        Validator loginValidator = new LoginValidator();
+
+        final ProgressDialog progressDialog1 = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog1.setMessage("Validating information...");
+        progressDialog1.show();
+
+        if (!loginValidator.validate(this)) {
+            progressDialog1.setMessage("Validating error...");
+            progressDialog1.show();
+            onLoginFailed();
+            onRestart();
+            progressDialog1.dismiss();
+            return false;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+        return true;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        onStop();
+        _loginButton.setEnabled(true);
+    }
+
+    public EditText get_emailText() {
+        return _emailText;
+    }
+
+    public EditText get_passwordText() {
+        return _passwordText;
+    }
+
+
 }
---
+++ app/src/main/java/fatproject/validation/LoginValidator.java
@@ -0,0 +1,39 @@
+package fatproject.validation;
+
+import android.support.v7.app.AppCompatActivity;
+
+import fatproject.activities.LoginActivity;
+
+/**
+ * Created by Komarenski Max on 02.01.2018.
+ */
+
+public class LoginValidator implements Validator{
+    public LoginValidator(){
+
+    }
+
+    @Override
+    public boolean validate(AppCompatActivity activity) {
+        boolean valid = true;
+
+        String email = ((LoginActivity) activity).get_emailText().getText().toString();
+        String password = ((LoginActivity) activity).get_passwordText().getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            ((LoginActivity) activity).get_emailText().setError("enter a valid email address");
+            valid = false;
+        } else {
+            ((LoginActivity) activity).get_emailText().setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            ((LoginActivity) activity).get_passwordText().setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            ((LoginActivity) activity).get_passwordText().setError(null);
+        }
+
+        return valid;
+    }
+}
