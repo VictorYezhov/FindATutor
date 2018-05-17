512e0b0a113b32964c7a33cc4c7cc70d67973539
MaxKomarenski <komarenski@ucu.edu.ua>
2018-05-17 16:12:45 +0300

    something new

--- app/src/main/java/fatproject/SendingForms/IdsForAppointment.java
+++ app/src/main/java/fatproject/SendingForms/IdsForAppointment.java
@@ -3,10 +3,12 @@ package fatproject.SendingForms;
 public class IdsForAppointment {
     private Long id_employ;
     private Long id_employer;
+    private Long question_id;
 
-    public IdsForAppointment(Long id_employ, Long id_employer) {
+    public IdsForAppointment(Long id_employ, Long id_employer, Long question_id) {
         this.id_employ = id_employ;
         this.id_employer = id_employer;
+        this.question_id = question_id;
     }
 
     public Long getId_employer() {
@@ -23,5 +25,13 @@ public class IdsForAppointment {
 
     public void setId_employ(Long id_employ) {
         this.id_employ = id_employ;
+    }
+
+    public Long getQuestion_id() {
+        return question_id;
+    }
+
+    public void setQuestion_id(Long question_id) {
+        this.question_id = question_id;
     }
 }
