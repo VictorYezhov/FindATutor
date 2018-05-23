94465d8a63562e68d22abe0608e65f7141fc11c1
VictorYezhov <vicyezh@gmail.com>
2018-05-23 16:20:43 +0300

    our url

--- app/src/main/java/fatproject/internet/URLs.java
+++ app/src/main/java/fatproject/internet/URLs.java
@@ -7,7 +7,8 @@ package fatproject.internet;
 public enum URLs {
 
     BASE_LOCKAL("http://10.0.2.2:8080/"),
-    BASE_REMOTE("http://138.68.79.221:8080/");
+    BASE_REMOTE("http://138.68.79.221:8080/"),
+    BASE_REMOTE_OUR("http://206.189.61.135:8080/");
 
     private String url;
 
